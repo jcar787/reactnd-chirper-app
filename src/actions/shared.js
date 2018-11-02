83fe0ece2b867532a0e4bab84f3cbcbfd6c86a66
Jeff C <blackone787@gmail.com>
2018-11-02 06:42:25 -0500

    Adding first actions to authed user

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,6 +1,9 @@
 import {getInitialData} from '../utils'
 import {receiveUsers} from './users'
 import {receiveTweets} from './tweets'
+import {setAuthedUser} from './authedUser'
+
+const AUTHED_ID = 'tylermcginnis'
 
 export const handleInitialData = () => {
     return dispatch => {
@@ -8,6 +11,7 @@ export const handleInitialData = () => {
         .then(({users, tweets})=> {
             dispatch(receiveUsers(users))
             dispatch(receiveTweets(tweets))
+            dispatch(setAuthedUser(AUTHED_ID))
         }) 
     }
 }
